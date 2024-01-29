c496a1f0a546432311f4bae91db8a05f2ce9c433
HengamehBaghiyan <hengamehb1382@gmail.com>
2024-01-30 02:05:15 +0330

    fix

--- proc.c
+++ proc.c
@@ -462,18 +462,12 @@ scheduler(void)
 
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
-    int numProccess=0;
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
-      if((p->state == RUNNABLE && p->is_thread == 0) || (p->state != RUNNABLE && p->count_thread > 0))
-        numProccess++;
-    if(numProccess>1){
-      cprintf("pid ptable proc: %s \n",ptable.proc->name);
-    }
+    
     
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
       if((p->state != RUNNABLE && p->count_thread==0)|| p->is_thread==1)
         continue;
-      cprintf("this process select:%d name: %s\n",p->pid,p->name);
+      //cprintf("this process select:%d name: %s\n",p->pid,p->name);
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
@@ -486,7 +480,7 @@ scheduler(void)
           c->proc = p;
           switchuvm(p);
           p->state = RUNNING;
-          cprintf("pid :%d name: %s\n",p->pid,p->name);
+          //cprintf("pid :%d name: %s\n",p->pid,p->name);
           swtch(&(c->scheduler), p->context);
           switchkvm();
 
@@ -504,7 +498,7 @@ scheduler(void)
               c->proc = p2;
               switchuvm(p2);
               p2->state = RUNNING;
-              cprintf("pid :%d index of threead: %d name: %s\n",p->pid,index,p->name);
+              //cprintf("pid :%d index of threead: %d name: %s\n",p->pid,index,p->name);
               swtch(&(c->scheduler), p2->context);
               switchkvm();
 
@@ -521,7 +515,7 @@ scheduler(void)
         c->proc = p;
         switchuvm(p);
         p->state = RUNNING;
-        cprintf("pid :%d name: %s\n",p->pid,p->name);
+        //cprintf("pid :%d name: %s\n",p->pid,p->name);
         swtch(&(c->scheduler), p->context);
         switchkvm();
 
